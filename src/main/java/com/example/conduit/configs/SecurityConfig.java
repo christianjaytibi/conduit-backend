829b4851981569985af667475449f418933be3fc
Christian Jay Tibi <christianjaytibi@gmail.com>
2026-02-25 13:51:26 +0800

    feat(security): add security scaffolding for pending API endpoints

---
+++ src/main/java/com/example/conduit/configs/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.example.conduit.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder(12);
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http){
+    http
+      .csrf(AbstractHttpConfigurer::disable)
+      .authorizeHttpRequests((auth) ->
+        auth
+          /* Login and registration
+          *  Authentication required to access user settings
+          * */
+          .requestMatchers(HttpMethod.POST, "/api/users/*").permitAll()
+          .requestMatchers("/api/user").authenticated()
+
+          /* Public access to profiles
+          *  Authentication required to follow another user
+          *  */
+          .requestMatchers(HttpMethod.GET, "/api/profiles/*").permitAll()
+          .requestMatchers("/api/profiles/**").authenticated()
+
+          /* Public access to read articles
+          *  Authentication required to write, favorite, and get feed
+          * */
+          .requestMatchers(HttpMethod.GET, "/api/articles/feed").authenticated()
+          .requestMatchers(HttpMethod.GET, "/api/articles/**").permitAll()
+          .requestMatchers("/api/articles/**").authenticated()
+
+          /* Public access to all tags */
+          .requestMatchers(HttpMethod.GET, "/api/tags").permitAll()
+
+          /* Allow other requests */
+          .anyRequest().permitAll()
+      )
+      .oauth2ResourceServer(oauth2 ->
+        oauth2.jwt(Customizer.withDefaults()))
+      .sessionManagement((session) ->
+        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+      );
+    return http.build();
+  }
+}
