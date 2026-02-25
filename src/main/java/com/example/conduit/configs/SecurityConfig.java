542b212269f8b01316005adceda67bb2ccf6e9ff
Christian Jay Tibi <christianjaytibi@gmail.com>
2026-02-25 19:43:59 +0800

    feat(security): expose authentication manager as a bean

--- src/main/java/com/example/conduit/configs/SecurityConfig.java
+++ src/main/java/com/example/conduit/configs/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.example.conduit.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -19,6 +21,11 @@ public class SecurityConfig {
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder(12);
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) {
+    return authConfig.getAuthenticationManager();
   }
 
   @Bean
