e89b56f499863db56221339e7066045033a69d79
Christian Jay Tibi <christianjaytibi@gmail.com>
2026-02-26 13:53:56 +0800

    fix(mapper): null fields mapped to user dto

--- src/main/java/com/example/conduit/mappers/UserMapper.java
+++ src/main/java/com/example/conduit/mappers/UserMapper.java
@@ -11,6 +11,14 @@ public interface UserMapper {
 
   @Mapping(target = "password", source = "encodedPassword")
   User entityFrom(RegisterUserRequest request, String encodedPassword);
+
+  @Mapping(target = "username", source = "profile.username")
+  @Mapping(target = "bio", source = "profile.bio")
+  @Mapping(target = "image", source = "profile.image")
   UserResponse toDto(User user);
+
+  @Mapping(target = "username", source = "user.profile.username")
+  @Mapping(target = "bio", source = "user.profile.bio")
+  @Mapping(target = "image", source = "user.profile.image")
   UserResponse toDtoWithToken(User user, String token);
 }
