a477d72122070cb2567379894e4b096c2084cbd6
Christian Jay Tibi <christianjaytibi@gmail.com>
2026-03-02 09:31:16 +0800

    refactor(profile): prototype method for retrieving profile returns optional profile projection

--- src/main/java/com/example/conduit/repositories/ProfileRepository.java
+++ src/main/java/com/example/conduit/repositories/ProfileRepository.java
@@ -1,11 +1,12 @@
 package com.example.conduit.repositories;
 
 import com.example.conduit.entities.Profile;
+import com.example.conduit.projections.ProfileView;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ProfileRepository extends JpaRepository<Profile, UUID> {
-  Optional<Profile> findByUsername(String username);
+  Optional<ProfileView> findByUsername(String username);
 }
